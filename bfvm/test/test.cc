19f5f59c33a3eacf3d978d8ff4d1b540d068753f
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2024-02-16 20:42:30 +0100

    Added new test, minor refactor in old tests

--- bfvm/test/test.cc
+++ bfvm/test/test.cc
@@ -3,14 +3,33 @@
 #include "bf_vm.hpp"
 
 
-TEST_CASE( "Executing plus sign N times returns N", "[base]" )
+TEST_CASE("Executing plus sign N times returns N", "[base]")
 {
     Bf_vm vm = Bf_vm();
     auto curr = GENERATE(1, 8, 42, 90);
-    std::string cmds = "";
-    for (auto i = 0; i < curr; ++i)
-        cmds += "+";
 
-    vm.execute(cmds);
+    vm.execute(std::string(curr, '+'));
     REQUIRE(vm.mem[0] == curr);
+}
+
+TEST_CASE("Executing plus sign X times, then Y times, returns X+Y", "[base]")
+{
+    Bf_vm vm = Bf_vm();
+    auto X = GENERATE(1, 8, 42, 90);
+    auto Y = GENERATE(30, 37, 3278, 9083);
+    vm.execute(std::string(X, '+'));
+    vm.execute(std::string(Y, '+'));
+    
+    REQUIRE(vm.mem[0] == X + Y);
+}
+
+TEST_CASE("Executing plus sign X times, then minus sign Y times, leave X-Y", "[base]")
+{
+    Bf_vm vm = Bf_vm();
+    auto X = GENERATE(39, 412, 800);
+    auto Y = GENERATE(90, 200, 302);
+    uint32_t expected = X - Y;
+
+    vm.execute(std::string(X, '+') + std::string(Y,'-'));
+    REQUIRE(vm.mem[0] == expected);
 }
