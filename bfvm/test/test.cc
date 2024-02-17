551ea46a02e62de433b15502c5b8305c6cf19986
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2024-02-17 14:02:42 +0100

    refactored tests a bit

--- bfvm/test/test.cc
+++ bfvm/test/test.cc
@@ -3,9 +3,6 @@
 #include "bf_vm.hpp"
 
 #include <string>
-#include <locale>
-#include <codecvt>
-
 
 TEST_CASE("Executing plus sign N times returns N", "[base]")
 {
@@ -19,10 +16,8 @@ TEST_CASE("Executing plus sign N times returns N", "[base]")
 TEST_CASE("Executing plus sign X times, then Y times, returns X+Y", "[base]")
 {
     Bf_vm vm = Bf_vm();
-    auto X = GENERATE(1, 8, 42, 90);
-    auto Y = GENERATE(30, 37, 3278, 9083);
-    vm.execute(std::string(X, '+'));
-    vm.execute(std::string(Y, '+'));
+    auto X = GENERATE(1, 8, 42, 90), Y = GENERATE(30, 37, 3278, 9083);
+    vm.execute(std::string(X + Y, '+'));
     
     REQUIRE(vm.mem[0] == X + Y);
 }
@@ -30,30 +25,25 @@ TEST_CASE("Executing plus sign X times, then Y times, returns X+Y", "[base]")
 TEST_CASE("Executing plus sign X times, then minus sign Y times, leave X-Y", "[base]")
 {
     Bf_vm vm = Bf_vm();
-    auto X = GENERATE(39, 412, 800);
-    auto Y = GENERATE(90, 200, 302);
-    uint32_t expected = X - Y;
+    auto X = GENERATE(39, 412, 800), Y = GENERATE(90, 200, 302);
 
     vm.execute(std::string(X, '+') + std::string(Y,'-'));
-    REQUIRE(vm.mem[0] == expected);
+    REQUIRE(vm.mem[0] == X - Y);
 }
 
 TEST_CASE("subtracing from zero returns wrapped int", "[base]")
 {
     Bf_vm vm = Bf_vm();
     auto X = GENERATE(39, 59, 127);
-    uint32_t expected = UINT32_MAX + 1 - X;
     vm.execute(std::string(X, '-'));
-    REQUIRE(vm.mem[0] == expected);
+    REQUIRE(vm.mem[0] == UINT32_MAX + 1 - X);
 }
 
 TEST_CASE("dot operator print out char", "[base]")
 {
     auto io = Bf_io_string_buff();
-    Bf_vm vm(io);
     char32_t X = GENERATE(U'A', U'B', U'c', U'f');
-    const uint32_t pluses = static_cast<uint32_t>(X);
-    vm.execute(std::string(pluses, '+') + ".");
+    Bf_vm(io).execute(std::string(static_cast<uint32_t>(X), '+') + ".");
 
     REQUIRE(io.out[0] == X);
 }
@@ -61,10 +51,8 @@ TEST_CASE("dot operator print out char", "[base]")
 TEST_CASE("adding, then moving pointer, pointer land on empty cell", "[base]")
 {
     auto io = Bf_io_string_buff();
-    Bf_vm vm(io);
-    auto offset = GENERATE(10, 20, 30, 40);
-    auto pluses = GENERATE(1, 2, 8, 91);
-    vm.execute(std::string(pluses, '+') + std::string(offset, '>') + ".");
+    auto offset = GENERATE(10, 20, 30, 40), pluses = GENERATE(1, 2, 8, 91);
+    Bf_vm(io).execute(std::string(pluses, '+') + std::string(offset, '>') + ".");
 
     REQUIRE(io.out[0] == 0);
 }
