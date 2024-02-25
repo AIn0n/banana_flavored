593f4b903547030f16fb89e18e1f72cdfcda4e63
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2024-02-25 16:01:31 +0100

    Work in progress -- building the parser

--- banana_flavored/include/parser.hpp
+++ banana_flavored/include/parser.hpp
@@ -1,9 +1,15 @@
 #include <string>
+#include "tokenizer.hpp"
 
 struct Parser {
-    const std::string code;
-    
-    Parser(const std::string &c) : code(c) {}
+    Tokenizer tokenizer;
+    Token current;
+    Token previous;
+
+    Parser(const std::string &c) : 
+        tokenizer(Tokenizer(c)),
+        current(Token("", Token_type::END_OF_FILE, 0)),
+        previous(Token("", Token_type::END_OF_FILE, 0)) {}
 
     std::string parse(void) {
         return "";
