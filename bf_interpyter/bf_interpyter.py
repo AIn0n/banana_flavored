18aaff8686e0bbc1909b0a69aff1438695655df1
AIn0n <55995692+AIn0n@users.noreply.github.com>
2026-05-18 17:02:34 +0200

    moved code from fuzzy_bf_experiments, precursor repo for this project

---
+++ bf_interpyter/bf_interpyter.py
@@ -0,0 +1,98 @@
+import numpy as np
+from enum import IntEnum
+
+np.seterr(over="ignore")  # ignore overflow warnings
+
+
+def get_bfvm_memory(mem_type: int, size: int) -> np.array:
+    size_to_constr = {
+        8: np.int8,
+        16: np.int16,
+        32: np.int32,
+        64: np.int64,
+    }
+    if mem_type not in size_to_constr.keys():
+        mem_type = 8
+    return np.array([0] * size, dtype=size_to_constr[mem_type])
+
+
+class BF_error:
+    def __init__(self, idx: int = -1, msg: str = "", suc: bool = True) -> None:
+        self.executed = suc
+        self.where = idx
+        self.msg = msg
+
+
+class BF_IO_handler:
+    def __init__(self) -> None:
+        self.input = []
+        self.output = []
+
+    def input_append(self, el: int) -> None:
+        self.input.append(el)
+
+    def input_pop(self):
+        return self.input.pop()
+
+    def output_append(self, el: int) -> None:
+        self.output.append(el)
+
+    def get_output(self) -> list:
+        return self.output
+
+
+def get_jump_table(commands: str) -> tuple[dict, BF_error]:
+    comeback_stack = []
+    jump_map = {}
+    for idx, val in enumerate(commands):
+        if val == "[":
+            comeback_stack.append(idx)
+        if val == "]":
+            last = comeback_stack.pop()
+            jump_map[last] = idx
+            jump_map[idx] = last
+    if len(comeback_stack) > 0:
+        idx = comeback_stack.pop()
+        return jump_map, BF_error(idx=idx, msg=f"unclosed bracked at {idx}", suc=False)
+
+    return jump_map, BF_error()
+
+
+def get_io_err_msg(idx):
+    return BF_error(idx=idx, msg=f"IO handler not defined, needed in {idx}", suc=False)
+
+
+class BF_interpreter:
+    def __init__(self, mem_type: int = 64, size: int = 4000):
+        self.memory = get_bfvm_memory(mem_type, size)
+        self.pointer = 0
+
+    def execute(self, commands: str, io_handler: BF_IO_handler = None) -> BF_error:
+        jump_map, err = get_jump_table(commands)
+        if not err.executed:
+            return err
+        if ("." in commands or "," in commands) and not io_handler:
+            return get_io_err_msg(max(commands.find("."), commands.find(",")))
+        n: int = 0
+        while n < len(commands):
+            match commands[n]:
+                case ".":
+                    io_handler.output_append(self.memory[self.pointer])
+                case ",":
+                    self.memory[self.pointer] = io_handler.input_pop()
+                case "+":
+                    self.memory[self.pointer] += 1
+                case "-":
+                    self.memory[self.pointer] -= 1
+                case ">":
+                    self.pointer += 1
+                case "<":
+                    self.pointer -= 1
+                case "[":
+                    if self.memory[self.pointer] == 0:
+                        n = jump_map[n]
+                case "]":
+                    n = jump_map[n]
+                    continue
+            n += 1
+        return BF_error()
---
+++ bf_interpyter/tests.py
@@ -0,0 +1,172 @@
+import pytest
+import numpy as np
+from bf_interpyter import *
+
+
+@pytest.mark.parametrize("N", [1, 23, 3849])
+def test_asking_for_N_size_returns_nparray_with_len_N(N):
+    memory = get_bfvm_memory(8, N)
+    assert len(memory) == N
+
+
+@pytest.mark.parametrize("N", [0, -100, -248])
+def test_asking_for_minus_N_size_returns_empty_list(N):
+    memory = get_bfvm_memory(8, N)
+    assert len(memory) == 0
+
+
+@pytest.mark.parametrize("N", [8, 16, 32, 64])
+def test_asking_for_N_bit_number_returns_number_which_roll_after_overflow(N):
+    memory = get_bfvm_memory(N, 1)
+    value_to_overflow = np.int64(2) ** (N - 1)
+    memory[0] += value_to_overflow
+    assert memory[0] == -value_to_overflow
+
+
+@pytest.mark.parametrize("N", [1, 2, 40, 80])
+def test_execute_plus_N_times_sign_returns_N(N):
+    interpreter = BF_interpreter()
+    interpreter.execute("+" * N)
+    assert interpreter.memory[0] == N
+
+
+@pytest.mark.parametrize("X,Y", [(1, 2), (8, 30), (21, 28)])
+def test_execute_plus_times_X_plus_times_Y_returns_XY(X, Y):
+    interpreter = BF_interpreter()
+    interpreter.execute("+" * X)
+    interpreter.execute("+" * Y)
+
+    assert interpreter.memory[0] == X + Y
+
+
+def test_giving_wrong_memory_type_defaults_to_8bit():
+    memory = get_bfvm_memory(9, 100)
+    assert memory.dtype == np.int8
+
+
+@pytest.mark.parametrize("N", [1, 10, 100])
+def test_all_commands_except_default_commands_set_are_ignored(N):
+    interpreter = BF_interpreter()
+    interpreter.execute("+" * N + "hello world!")
+    assert interpreter.memory[0] == N
+
+
+@pytest.mark.parametrize("X,Y", [(2, 1), (30, 8), (28, 21)])
+def test_plus_X_minus_Y_returns_X_minus_Y(X, Y):
+    interpreter = BF_interpreter()
+    interpreter.execute("+" * X)
+    interpreter.execute("-" * Y)
+    assert interpreter.memory[0] == X - Y
+
+
+@pytest.mark.parametrize("X,Y", [(2, 1), (30, 8), (28, 21)])
+def test_move_right_X_plus_Y_returns_Y_in_X(X, Y):
+    interpreter = BF_interpreter()
+    interpreter.execute(">" * X)
+    interpreter.execute("+" * Y)
+    assert interpreter.memory[X] == Y
+
+
+@pytest.mark.parametrize("X,Y,Z", [(3, 1, 10), (30, 8, 32), (28, 21, 9)])
+def test_move_right_X_move_left_Y_plus_Z_returns_Z_in_X_minus_Y(X, Y, Z):
+    interpreter = BF_interpreter()
+    interpreter.execute(">" * X)
+    interpreter.execute("<" * Y)
+    interpreter.execute("+" * Z)
+    assert interpreter.memory[X - Y] == Z
+
+
+@pytest.mark.parametrize("N", [3, 7, 12, 32])
+def test_loop_to_copy_to_next_cell(N):
+    interpreter = BF_interpreter()
+    interpreter.execute("+" * N)
+    interpreter.execute("[->+<]")
+    assert interpreter.memory[interpreter.pointer + 1] == N
+    assert interpreter.memory[interpreter.pointer] == 0
+
+
+@pytest.mark.parametrize("N", [1, 5, 49, 348])
+def test_nested_loop_to_move_pointers_returns_state_with_moved_pointer(N):
+    interpreter = BF_interpreter()
+    # init first cell at the tape with iterator value
+    interpreter.execute("+" * N)
+    interpreter.execute(
+        """
+    [
+        [->+<] ## move iterator one to the right
+        >- ## move pointer and decrement iterator
+    ] # end loop
+    """
+    )
+    assert interpreter.pointer == N
+    assert not interpreter.memory.any()
+
+
+@pytest.mark.parametrize("N", [1, 16, 48, 89])
+def test_nested_loops_without_closing_returns_bf_error_with_trace(N):
+    interpreter = BF_interpreter()
+    err = interpreter.execute(
+        ("+" * N)
+        + """[
+        [->+<] ## move iterator one to the right
+        >- ## move pointer and decrement iterator
+    #  lack of loop closing
+    """
+    )
+    # message should be somehow meaningful
+    assert "unclosed bracked" in err.msg
+    # error should be shown at index bracked right after iterator init
+    assert err.where == N
+    # error should have information about if code was executed or not
+    assert err.executed == False
+
+
+@pytest.mark.parametrize("N", [1, 16, 48, 49])
+def test_user_input_should_move_pointer_by_N(N):
+    interpeter = BF_interpreter()
+    io_handler = BF_IO_handler()
+    io_handler.input_append(N)
+    interpeter.execute(
+        """
+    ,[
+        [->+<] ## move iterator one to the right
+        >- ## move pointer and decrement iterator
+    ] # end loop
+    """,
+        io_handler=io_handler,
+    )
+    assert interpeter.pointer == N
+
+
+def test_output_command_without_defined_handler_returns_error():
+    interpeter = BF_interpreter()
+    bf_code = """
++++ # init first cell with some number
+>++ # init the second cell with another one
+[->+<] # Copy second cell into the third 
+<[->>+<<] # do the same with the first one
+>>. # Show addition result to the user"""
+
+    err = interpeter.execute(bf_code)
+
+    assert err.executed == False
+    assert "IO handler not defined" in err.msg
+    # error happens when program first time meet interaction with IO
+    assert err.where == bf_code.find(".")
+
+
+@pytest.mark.parametrize("X,Y", [(10, 20), (13, 17), (49, 9)])
+def test_addition_of_two_given_by_user_numbers_returns_sum_in_output(X, Y):
+    interpeter = BF_interpreter()
+    io_handler = BF_IO_handler()
+    io_handler.input_append(X)
+    io_handler.input_append(Y)
+    bf_code = """
+, # init first cell with some number
+>, # init the second cell with another one
+[->+<] # Copy second cell into the third 
+<[->>+<<] # do the same with the first one
+>>. # Show addition result to the user"""
+
+    interpeter.execute(bf_code, io_handler=io_handler)
+    assert io_handler.get_output()[0] == X + Y
